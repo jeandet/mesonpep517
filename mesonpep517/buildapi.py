be3e2beeaa75c595a015b2a6440d7e3bd9d213f8
Thibault Saunier <tsaunier@igalia.com>
2020-07-15 12:05:08 -0400

    Fix spelling issue

--- mesonpep517/buildapi.py
+++ mesonpep517/buildapi.py
@@ -91,7 +91,7 @@ class Config:
             del options[field]
 
         for field, desc in options.items():
-            if not desc.get('optionnal'):
+            if not desc.get('optional'):
                 raise RuntimeError("%s is mandatory in the `[tool.mesonpep517.metadata] section but was not found" % field)
 
     def __introspect(self, introspect_type):
